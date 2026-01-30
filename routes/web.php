5d9d858fdcb469356faeabb78745468a83a46ef7
andrecaindaa <andre.cainda@trainee.inovcorp.com>
2026-01-30 17:22:40 +0000

    Refactor routes: normalize Deals flow, Kanban as index and remove duplicates

--- routes/web.php
+++ routes/web.php
@@ -1,43 +1,21 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\EntityController;
 use App\Http\Controllers\PersonController;
 use App\Http\Controllers\DealController;
+use App\Http\Controllers\DealProposalController;
 use App\Http\Controllers\CalendarEventController;
 
-Route::middleware(['auth'])->group(function () {
-    Route::get('/calendar', [CalendarEventController::class, 'index'])
-        ->name('calendar.index');
-
-    Route::post('/calendar', [CalendarEventController::class, 'store'])
-        ->name('calendar.store');
-});
-
-
-Route::get('/deals/create', [DealController::class, 'create'])
-    ->name('deals.create');
-
-
-Route::middleware(['auth'])->group(function () {
-    Route::get('/deals', [DealController::class, 'index'])->name('deals.index');
-    Route::post('/deals', [DealController::class, 'store'])->name('deals.store');
-    Route::patch('/deals/{deal}/stage', [DealController::class, 'updateStage']);
-});
-
-
-Route::middleware(['auth'])->group(function () {
-    Route::resource('people', PersonController::class);
-});
-
-Route::middleware(['auth'])->group(function () {
-    Route::resource('entities', EntityController::class);
-});
-
-
+/*
+|--------------------------------------------------------------------------
+| Public
+|--------------------------------------------------------------------------
+*/
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
@@ -47,14 +25,72 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth'])->name('dashboard');
+/*
+|--------------------------------------------------------------------------
+| Authenticated Area
+|--------------------------------------------------------------------------
+*/
+Route::middleware(['auth'])->group(function () {
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    /*
+    | Dashboard
+    */
+    Route::get('/dashboard', fn () => Inertia::render('Dashboard'))
+        ->name('dashboard');
+
+    /*
+    | Deals (Negócios)
+    */
+    Route::get('/deals', [DealController::class, 'index'])
+        ->name('deals.index');
+
+    Route::get('/deals/create', [DealController::class, 'create'])
+        ->name('deals.create');
+
+    Route::post('/deals', [DealController::class, 'store'])
+        ->name('deals.store');
+
+    Route::get('/deals/{deal}', [DealController::class, 'show'])
+        ->name('deals.show');
+
+    Route::patch('/deals/{deal}/stage', [DealController::class, 'updateStage'])
+        ->name('deals.stage');
+
+    /*
+    | Deal Proposals
+    */
+    Route::post('/deals/{deal}/proposals', [DealProposalController::class, 'store'])
+        ->name('deals.proposals.store');
+
+    Route::post('/proposals/{proposal}/send', [DealProposalController::class, 'send'])
+        ->name('proposals.send');
+
+    /*
+    | Calendar
+    */
+    Route::get('/calendar', [CalendarEventController::class, 'index'])
+        ->name('calendar.index');
+
+    Route::post('/calendar', [CalendarEventController::class, 'store'])
+        ->name('calendar.store');
+
+    /*
+    | Entities & People
+    */
+    Route::resource('entities', EntityController::class);
+    Route::resource('people', PersonController::class);
+
+    /*
+    | Profile
+    */
+    Route::get('/profile', [ProfileController::class, 'edit'])
+        ->name('profile.edit');
+
+    Route::patch('/profile', [ProfileController::class, 'update'])
+        ->name('profile.update');
+
+    Route::delete('/profile', [ProfileController::class, 'destroy'])
+        ->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
